5b5a5d6b3e134f7ed042dd658bd786c545c059f6
Janet Awino <janetkeyo@gmail.com>
2024-12-19 08:59:31 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header(){
+    return(
+        <header>
+            <img src="/globe.png" alt="globe icon" />
+            <span>My travel journal</span>
+        </header>
+    )
+}
